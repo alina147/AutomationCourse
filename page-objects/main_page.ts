58f3cdb05c01b741d9d0ffa02f6d272e3803856c
alinayer <alina.yermolovich@sumup.com>
2024-02-04 21:15:07 +0200

    updates

--- page-objects/main_page.ts
+++ page-objects/main_page.ts
@@ -27,7 +27,7 @@ export class MainPage {
         this.search_result_locator = page.locator("//a[@class='recommended-product__coverLink']");
         this.productToList = page.getByText("+ Add");
         this.goToCheckout = page.getByRole('link', { name: 'Checkout' });
-        this.testURL = 'https://test.ua/en';
+        this.testURL = 'https://perlahelsa.ua/en';
         this.banner_to_be_visible = '//html/body/div[2]/div/div[1]/header/div[3]/nav/ul/li[4]/a[1]';
         this.massive_of_tags = [/^Skin and hair$/, /^Mood and sleep$/, /^Immunity$/, /^Vitality$/];
         this.loginField = page.getByPlaceholder('E-mail or phone number');
@@ -39,6 +39,6 @@ export class MainPage {
         this.bannerTitle=page.getByRole('heading', { name: 'Get -10% off your first order' });
     }
     async goto() {
-        await this.page.goto('https://test.ua/en');
+        await this.page.goto('https://perlahelsa.ua/en');
     }
 }
